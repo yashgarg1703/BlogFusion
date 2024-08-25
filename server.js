51ff85b8c68397dac14bff9a006448d1971b3132
Yash <yashgarg1703@gmail.com>
2024-08-25 23:50:47 +0530

    CORS  commit-2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const errorHandler = require("./middlewares/error-handling-midware")
 
 var corsOptions = {
     //  origin: 'http://localhost:5173'
-    origin: 'https://deft-trifle-2d4a63.netlify.app',
+    origin: 'https://earnest-squirrel-48b519.netlify.app',
     methods:'POST,GET,DELETE,PATCH,HEAD,PUT',
     credentials:true
   }
